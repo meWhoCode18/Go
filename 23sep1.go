ebc18c285a6a34834739801682765d4d7e4310e3
Krish Panchal <226514194+meWhoCode18@users.noreply.github.com>
2026-09-23 18:11:55 +0530

    Add initial implementation for message cost calculation

---
+++ 23sep1.go
@@ -0,0 +1,14 @@
+//Learning from boot.dev
+//CH1 L5 WHY GO?
+
+
+package main
+
+import "fmt"
+
+func main() {
+	numMessagesFromDoris := 72
+	costPerMessage := .02
+	totalCost := costPerMessage * float64(numMessagesFromDoris)
+	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+}
