ed23b8571083fda2ed75fd684dd6c0f22d5f4dde
Krish Panchal <226514194+meWhoCode18@users.noreply.github.com>
2026-09-24 21:03:19 +0530

    Add error handling explanations in CH1L7.go
    Added comments explaining compilation and runtime errors.

---
+++ CH1L7.go
@@ -0,0 +1,16 @@
+// CH 1 L7 the computation process 
+
+//Compilation errors happen when the code is compiled. They're the good kind: a program with a compiler error won't build, so it can't ship to production.
+
+
+//Runtime errors happen while the program is running. They're worse because they can crash a program that's already in use.
+
+package main
+
+
+
+import "fmt"
+
+func main() {
+	fmt.Println("The compiled textio server is starting") // In the lesson there were errors in the boot.dev that I have to debug 
+}
